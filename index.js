50fcd0da94bfe234051a01147599fe57091fabd7
thatguyKacper <kacper.m.wojtowicz@gmail.com>
2025-04-06 19:01:00 +0200

    add user input read

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
 import { writeFileSync } from 'fs'
+import { createInterface } from 'readline'
 
 const content = 'Test content'
+
+const readline = createInterface({
+  input: process.stdin,
+  output: process.stdout,
+})
+
+const readLineAsync = (message) =>
+  new Promise((resolve) => readline.question(message, resolve))
 
 try {
   writeFileSync('./test.txt', content)
